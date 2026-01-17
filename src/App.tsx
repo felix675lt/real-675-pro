745db280b52bcf730f6061af03012a8f596b6c86
felix675lt <contact@studio675.xyz>
2026-01-18 01:57:28 +0900

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ 
+      backgroundColor: 'red', 
+      height: '100vh', 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center',
+      color: 'white',
+      fontSize: '2rem'
+    }}>
+      src 폴더 연결 성공! (빨간 화면)
+    </div>
+  );
+}
+
+export default App;
