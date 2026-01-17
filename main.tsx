97e904cdaa3003a5c7689ee79877c76a0e1ff70b
felix675lt <contact@studio675.xyz>
2026-01-18 01:33:33 +0900

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx' // 여기가 ./services/App.tsx 로 되어있으면 안됩니다!
-import './index.css'
+import App from './App.tsx'
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
