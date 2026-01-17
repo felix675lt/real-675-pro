496193fc4d729c486e6ee31e111a875301a1ce8e
felix675lt <contact@studio675.xyz>
2026-01-18 01:03:13 +0900

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.tsx' // 여기가 ./services/App.tsx 로 되어있으면 안됩니다!
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
