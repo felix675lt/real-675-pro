0d54de6a687ef5c62124bbe5504b6f3ef8a6fefc
felix675lt <contact@studio675.xyz>
2026-01-18 02:00:19 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ function App() {
   return (
     <div style={{ 
       backgroundColor: 'red', 
+      width: '100vw', 
       height: '100vh', 
       display: 'flex', 
       justifyContent: 'center', 
       alignItems: 'center',
       color: 'white',
-      fontSize: '2rem'
+      fontSize: '30px',
+      fontWeight: 'bold'
     }}>
-      src 폴더 연결 성공! (빨간 화면)
+      드디어 연결 성공! (빨간 화면)
     </div>
   );
 }
